01a56766bb16396e86599c56b07dce39c141aedb
OldGrand <Sergey.sipov.150699@gmail.com>
2019-11-29 12:35:15 +0300

    Test

--- CarShowroomWorkstation/CarShowroomWorkstation/PersonalDataInputWindow.xaml.cs
+++ CarShowroomWorkstation/CarShowroomWorkstation/PersonalDataInputWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,11 +20,48 @@ namespace CarShowroomWorkstation
     /// </summary>
     public partial class PersonalDataInputWindow : Window
     {
+        private Regex regex;
         public PersonalDataInputWindow()
         {
             InitializeComponent();
             BirthDatePicker.DisplayDate = DateTime.Now;
             BirthDatePicker.Text = DateTime.Now.ToString();
+            EndRegistrationButton.IsEnabled = false;
+            
+
+            BirthDatePicker.Loaded += delegate
+            {
+                var textBox = (TextBox)BirthDatePicker.Template.FindName("PART_TextBox", BirthDatePicker);
+                textBox.Background = BirthDatePicker.Background;
+                textBox.Foreground = new SolidColorBrush(Colors.White);
+                textBox.IsEnabled = false;
+            };
+        }
+
+        public void PassportnNumTextChanged(object sender, EventArgs e)
+        {
+            regex = new Regex(@"^[A-Z]{2}\d{6}$");
+            if (regex.IsMatch(PassportNumTextBox.Text))
+            {
+                EndRegistrationButton.IsEnabled = true;
+            }
+            else
+            {
+                EndRegistrationButton.IsEnabled = false;
+            }
+        }
+
+        public void PhoneNumTextChanged(object sender, EventArgs e)
+        {
+            regex = new Regex(@"^([+]37529)?\d{7}\b$");
+            if (regex.IsMatch(PassportNumTextBox.Text))
+            {
+                EndRegistrationButton.IsEnabled = true;
+            }
+            else
+            {
+                EndRegistrationButton.IsEnabled = false;
+            }
         }
     }
 }
--- CarShowroomWorkstation/CarShowroomWorkstation/Properties/Resources.Designer.cs
+++ CarShowroomWorkstation/CarShowroomWorkstation/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace CarShowroomWorkstation.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap PersonalDataBackGround {
+            get {
+                object obj = ResourceManager.GetObject("PersonalDataBackGround", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap RegistrationBackGround {
             get {
                 object obj = ResourceManager.GetObject("RegistrationBackGround", resourceCulture);
